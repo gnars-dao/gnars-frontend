a1e19b75c59ee8e8956d81e4edf0aac5fd0ed6e3
Sam Mason de Caires <sam@rainbow.me>
2022-10-04 20:10:22 +0100

    Add some test text to kickoff build

--- apps/www/components/LatestAuction.tsx
+++ apps/www/components/LatestAuction.tsx
@@ -3,5 +3,5 @@ import useLatestAuction from "hooks/useLatestAuction"
 export default function LatestAuction() {
   const { isLoading, data } = useLatestAuction()
   console.log(data)
-  return <div></div>
+  return <div>heyyyy</div>
 }
