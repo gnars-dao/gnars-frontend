c21a628344aba5aaf2b7cd52ce3d9a1fb2031748
Volky <volkyeth@proton.me>
2023-03-16 08:45:10 -0300

    Update countdown when auction is extended

--- packages/webapp/hooks/useAuctionTimeLeft.ts
+++ packages/webapp/hooks/useAuctionTimeLeft.ts
@@ -24,7 +24,7 @@ export default function useAuctionTimeLeft(endTimestamp?: number) {
     }
     newAuctionTimeLeft += `${duration.seconds}s`
     setAuctionTimeLeft(newAuctionTimeLeft)
-  }, [])
+  }, [endTimestamp])
 
   useEffect(() => {
     if (!endTimestamp || isInPast) {
