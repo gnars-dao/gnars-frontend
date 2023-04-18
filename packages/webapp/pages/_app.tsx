5ce4e5be20beea0863c21f78f150789310372426
Volky <volkyeth@pm.me>
2023-04-18 15:51:03 -0300

    Disable vercel analytics debug

--- packages/webapp/pages/_app.tsx
+++ packages/webapp/pages/_app.tsx
@@ -39,7 +39,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Component {...pageProps} />
                 <Footer />
               </VStack>
-              <Analytics />
+              <Analytics debug={false} />
             </DarkMode>
           </QueryClientProvider>
         </ConnectKitProvider>
