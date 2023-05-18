156af374f94c1aaa343bdd178240a3b5932d1f8a
Volky <volkyeth@pm.me>
2023-05-18 05:41:52 -0300

    remove app folder

--- packages/webapp/next.config.js
+++ packages/webapp/next.config.js
@@ -2,16 +2,14 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  transpilePackages: ["react-icons"],
   webpack: (config) => {
-
     config.module = {
       ...config.module,
       exprContextCritical: false,
-    };
+    }
 
     return config
-  }
+  },
 }
 
 module.exports = nextConfig
