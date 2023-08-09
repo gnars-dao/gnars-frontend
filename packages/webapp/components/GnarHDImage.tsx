9c1771e6884b9ac7d7369ff96b4061555cea114b
Volky <volkyeth@pm.me>
2023-08-09 15:35:39 -0300

    Fix Gnar HD download

--- packages/webapp/components/GnarHDImage.tsx
+++ packages/webapp/components/GnarHDImage.tsx
@@ -1,5 +1,4 @@
 import { Image, ImageProps, forwardRef } from "@chakra-ui/react"
-import dynamic from "next/dynamic"
 import { useMemo } from "react"
 import { getGnarsHdLoImageUrl } from "utils/gnarsHD"
 
@@ -14,9 +13,7 @@ export interface GnarHDImageProps extends ImageProps {
   }
 }
 
-export const GnarHDImage = dynamic(() => Promise.resolve(StaticGnarHDImage), { ssr: false })
-
-const StaticGnarHDImage = forwardRef<GnarHDImageProps, "img">(({ seed, gnarId, ...props }, ref) => {
+export const GnarHDImage = forwardRef<GnarHDImageProps, "img">(({ seed, gnarId, ...props }, ref) => {
   const gnarsHdUrl = useMemo(() => getGnarsHdLoImageUrl(seed), [seed])
   return <Image src={gnarsHdUrl} alt={`Gnar HD #${gnarId}`} ref={ref} {...props} />
 })
