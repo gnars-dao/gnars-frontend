918ccea490756a4f340d7c4e89cfcab40209c389
Matt Oldfield <mattoldfieldweb@gmail.com>
2024-04-16 13:36:19 -0400

    refactor(update-packages): useQuery refactor

--- packages/webapp/hooks/useWalletHDGnars.ts
+++ packages/webapp/hooks/useWalletHDGnars.ts
@@ -5,9 +5,10 @@ export const walletHDGnarsQueryKey = "hdGnarsClaimStatus"
 
 export const useWalletHDGnars = (address?: string) => {
   const sdk = getBuiltGraphSDK()
-  return useQuery(
-    [walletHDGnarsQueryKey, address],
-    () => {
+  // https://tanstack.com/query/latest/docs/framework/react/guides/migrating-to-v5#supports-a-single-signature-one-object
+  return useQuery({
+    queryKey: [walletHDGnarsQueryKey, address],
+    queryFn: () => {
       if (!address) {
         return []
       }
@@ -17,8 +18,6 @@ export const useWalletHDGnars = (address?: string) => {
         .then((r) => r.gnars || [])
         .then((gnars) => gnars.map((g) => ({ ...g, wasClaimed: address.toLowerCase() === g.hdOwner.toLowerCase() })))
     },
-    {
-      refetchInterval: 12000,
-    }
-  )
+    refetchInterval: 12000,
+})
 }
