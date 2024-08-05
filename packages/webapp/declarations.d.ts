3789bc79c0ddf803514ad7c0ce7203c6e180855c
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-08-05 09:01:08 -0500

    build working

--- packages/webapp/declarations.d.ts
+++ packages/webapp/declarations.d.ts
@@ -3,6 +3,8 @@ declare module "connectkit" {
   const ConnectKit: any
   const ConnectKitProvider: any
   const getDefaultConfig: any
+  const ConnectKitButton: any
+  const useModal: any
   export default ConnectKit
-  export { ConnectKitProvider, getDefaultConfig }
+  export { ConnectKitProvider, getDefaultConfig, ConnectKitButton, useModal }
 }
