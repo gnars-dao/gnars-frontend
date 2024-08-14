04bd7495f8098cf7edb660c5ce012a943af0d4d3
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-08-14 14:20:21 -0500

    typo

--- packages/webapp/graphql/ssr.client.ts
+++ packages/webapp/graphql/ssr.client.ts
@@ -2,5 +2,5 @@ import { GraphQLClient } from 'graphql-request';
 
 import { env } from '@env/server';
 
-// this hits the graphql api exposed in the /app/graphql.ts file
+// this hits the graphql api exposed in the pages/api/graphql.ts file
 export const graphQLClient = new GraphQLClient(env.ROOT_URI);
