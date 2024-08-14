cb58682bd9741647f8dadc91e5f0a9738a3df240
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-08-14 16:38:13 -0500

    typed env vars are cool

--- packages/webapp/graphql/ssr.client.ts
+++ packages/webapp/graphql/ssr.client.ts
@@ -2,4 +2,4 @@ import { GraphQLClient } from 'graphql-request';
 import env from '@env/server'
 
 // this hits the graphql api exposed in the pages/api/graphql.ts file
-export const graphQLClient = new GraphQLClient(env.ROOT_URI as string);
+export const graphQLClient = new GraphQLClient(env.ROOT_URI);
