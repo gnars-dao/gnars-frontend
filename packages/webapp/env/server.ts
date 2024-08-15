a9d6414a352fb47c625853b2b953e80753599676
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-08-15 01:54:30 -0500

    move back to goldsky

--- packages/webapp/env/server.ts
+++ packages/webapp/env/server.ts
@@ -4,12 +4,12 @@ import { z } from 'zod';
 export default createEnv({
   server: {
     PRIVATE_GRAPH_LAYER_1_API_KEY: z.string().min(32).max(32),
-    // PRIVATE_GOLDSKY_BASE_API_URL: z.string().url(),
+    PRIVATE_GOLDSKY_L1_SUBGRAPH: z.string().url(),
     ROOT_URI: z.string().url().default('http://localhost:3000/api/graphql'),
   },
   runtimeEnv: {
     PRIVATE_GRAPH_LAYER_1_API_KEY: process.env.PRIVATE_GRAPH_LAYER_1_API_KEY,
-    // PRIVATE_GOLDSKY_BASE_API_URL: process.env.PRIVATE_GOLDSKY_BASE_API_URL,
+    PRIVATE_GOLDSKY_L1_SUBGRAPH: process.env.PRIVATE_GOLDSKY_L1_SUBGRAPH,
     ROOT_URI: process.env.ROOT_URI
   },
 });
