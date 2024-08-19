13e8e4f567903ce78dc00a629535b3196521eebc
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-08-19 12:28:40 -0500

    remove comment

--- packages/webapp/graphql/ssr.client.ts
+++ packages/webapp/graphql/ssr.client.ts
@@ -1,4 +1,3 @@
 import { GraphQLClient } from 'graphql-request';
 
-// this hits the graphql api exposed in the pages/api/graphql.ts file
 export const graphQLClient = new GraphQLClient(process.env.GRAPHQL_API as string);
